a150038787f909df2257e6081aeecee0b8d399e9
embeNG <116657377+Embeng123@users.noreply.github.com>
2024-10-13 19:39:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 // worker.js
 import { connect } from "cloudflare:sockets";
 var listProxy = [
-  { "path": "/1", "proxy": "172.232.252.101:587" },
-  { "path": "/2", "proxy": "8.215.23.33:587" },
-  { "path": "/3", "proxy": "129.150.50.63:443" },
+  { "path": "/1", "proxy": "172.232.252.101" },
+  { "path": "/2", "proxy": "8.215.23.33" },
+  { "path": "/3", "proxy": "129.150.50.63" },
   { "path": "/4", "proxy": "203.194.112.119" },
   { "path": "/5", "proxy": "35.219.50.99" },
   { "path": "/6", "proxy": "172.232.238.169" },
-  { "path": "/7", "proxy": "172.232.234.119:587" },
-  { "path": "/8", "proxy": "103.180.193.56:443" },
-  { "path": "/9", "proxy": "101.50.0.114:2053" },
-  { "path": "/10", "proxy": "101.50.0.114:8443" },
-  { "path": "/11", "proxy": "103.133.223.51:2096" },
-  { "path": "/12", "proxy": "104.248.145.216:443" },
-  { "path": "/13", "proxy": "103.133.223.52:2096" },
-  { "path": "/14", "proxy": "103.232.213.211:27043" },
-  { "path": "/15", "proxy": "103.133.223.50:2096" },
-  { "path": "/16", "proxy": "104.40.244.228:443" },
-  { "path": "/17", "proxy": "109.120.135.150:443" },
-  { "path": "/18", "proxy": "104.215.16.121:443" },
-  { "path": "/19", "proxy": "109.120.134.15:443" },
-  { "path": "/20", "proxy": "109.120.132.218:443" },
-  { "path": "/21", "proxy": "103.99.178.62:51794" },
-  { "path": "/22", "proxy": "103.99.179.39:443" },
-  { "path": "/23", "proxy": "109.120.134.133:443" },
-  { "path": "/24", "proxy": "109.120.132.147:443" },
-  { "path": "/25", "proxy": "109.120.133.242:443" },
-  { "path": "/26", "proxy": "109.120.133.8:443" },
-  { "path": "/27", "proxy": "103.99.178.62:13241" },
-  { "path": "/28", "proxy": "109.120.132.136:443" },
-  { "path": "/29", "proxy": "103.99.178.62:31940" },
-  { "path": "/30", "proxy": "104.210.3.231:8443" }
+  { "path": "/7", "proxy": "172.232.234.119" },
+  { "path": "/8", "proxy": "103.180.193.56" },
+  { "path": "/9", "proxy": "101.50.0.114" },
+  { "path": "/10", "proxy": "101.50.0.114" },
+  { "path": "/11", "proxy": "103.133.223.51" },
+  { "path": "/12", "proxy": "104.248.145.216" },
+  { "path": "/13", "proxy": "103.133.223.52" },
+  { "path": "/14", "proxy": "103.232.213.211" },
+  { "path": "/15", "proxy": "103.133.223.50" },
+ 
   // Tambah proxy lain sesuai kebutuhan
 ];
 var apiCheck = "https://ipwho.is/json/?ip=";
@@ -125,10 +111,10 @@ async function getAllConfigVless(hostName) {
       const pathFixed = encodeURIComponent(path);
       const vlessTls = `vless://Israel=Babi@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=${pathFixed}#${data.isp} (${data.country_code})`;
       const vlessXCL = `vless://Israel=Babi@ava.game.naver.com:443?encryption=none&security=tls&sni=ava.game.naver.com.${hostName}&fp=randomized&type=ws&host=ava.game.naver.com.${hostName}&path=${pathFixed}# Xcl ${data.isp}(${data.country_code})`;
-      const vlessNtls = `vless://Israel=Babi@${hostName}:80?path=${pathFixed}&security=none&encryption=none&host=${hostName}&fp=randomized&type=ws&sni=${hostName}#${data.isp} (${data.country_code})`;
+      const vlessGcore = `vless://Israel=Babi@gcore.embeng.us.kg:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=gcore.embeng.us.kg&path=${pathFixed}#${data.isp} (${data.country_code})`;
       const vlessTlsFixed = vlessTls.replace(/ /g, "+");
       const vlessXCLFixed = vlessXCL.replace(/ /g, "+");
-      const vlessNtlsFixed = vlessNtls.replace(/ /g, "+");
+      const vlessGcoreFixed = vlessGcore.replace(/ /g, "+");
       const clashConfTls = `- name: ${data.isp} (${data.country_code})
   server: ${hostName}
   port: 443
@@ -144,25 +130,26 @@ async function getAllConfigVless(hostName) {
     path: ${path}
     headers:
       Host: ${hostName}`;
-      const clashConfNtls = `- name: ${data.isp} (${data.country_code})
-  server: ${hostName}
-  port: 80
+      const clashConfGcore = `- name: ${data.isp} (${data.country_code})
+  server: gcore.embeng.us.kg
+  port: 443
   type: vless
   uuid: Israel=Babi
   cipher: auto
-  tls: false
+  tls: true
   udp: true
   skip-cert-verify: true
   network: ws
+  servername: gcore.embeng.us.kg
   ws-opts:
     path: ${path}
     headers:
-      Host: ${hostName}`;
+      Host: gcore.embeng.us.kg`;
       clashConfigs += `<div style="display: none;">
    <textarea id="clashTls${path}">${clashConfTls}</textarea>
  </div>
 <div style="display: none;">
-   <textarea id="clashNtls${path}">${clashConfNtls}</textarea>
+   <textarea id="clashNtls${path}">${clashConfGcore}</textarea>
  </div>
 <div class="config-section">
     <p><strong>ISP:${data.isp} (${data.country_code})</strong> </p>
@@ -179,7 +166,7 @@ async function getAllConfigVless(hostName) {
             <hr />
             <div class="config-block">
                 <h3>NTLS:</h3>
-                <p class="config">${clashConfNtls}</p>
+                <p class="config">${clashConfGcore}</p>
                 <button class="button" onclick='copyClash("clashNtls${path}")'><i class="fa fa-clipboard"></i>Copy</button>
             </div>
         </div>
@@ -202,8 +189,8 @@ async function getAllConfigVless(hostName) {
             <hr />
             <div class="config-block">
                 <h3>NTLS:</h3>
-                <p class="config">${vlessNtlsFixed}</p>
-                <button class="button" onclick='copyToClipboard("${vlessNtlsFixed}")'><i class="fa fa-clipboard"></i>Copy</button>
+                <p class="config">${vlessGcoreFixed}</p>
+                <button class="button" onclick='copyToClipboard("${vlessGcoreFixed}")'><i class="fa fa-clipboard"></i>Copy</button>
             </div>
             <hr />
             <div class="config-block">
@@ -485,8 +472,6 @@ async function getAllConfigVless(hostName) {
         <div class="config-section">
         
         <strong>Note: </strong><br>
-        \u2730 workers cf: embeng.us.kg<br>
-        \u2730 pages cf: embeng.ggff.net<br>
         \u2730 Utk wilcard bisa request!<br>
         </div>
         <hr class="config-divider" />
